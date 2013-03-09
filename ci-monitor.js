e41c5b06ddb5673f5303b7850c9394af7aaeb16e
Jamie Cook <jimi.cook@gmail.com>
2013-03-09 00:58:29 -0500

    refactor

--- ci-monitor.js
+++ ci-monitor.js
@@ -36,7 +36,7 @@ function getBody(content)
     return content.slice(x + 1, y);
 }
 
-function convert_service_entry_to_tr(data, left_or_right) {
+function convert_service_entry_to_tr(data, left_or_right, height) {
   // console.log(JSON.stringify(data));
   if (data.notifications_enabled == "0") {
     return ""
@@ -44,7 +44,8 @@ function convert_service_entry_to_tr(data, left_or_right) {
   
   var host_and_status = data.plugin_output.split(' - ')[0]
   var status          = "status-" + convert_integer_status_to_string(data.current_state)
-  return "<div class='host-entry " + status + " " + left_or_right + "'>" +
+  var class_str       = "class='host-entry " + status + " " + left_or_right + "' "
+  return "<div " + class_str + height + ">" +
     data.current_state + " " + data['host'] + ' ' + host_and_status + 
   "</div>"
 }
@@ -53,13 +54,14 @@ function toggle_left_right(left_or_right) {
   return left_or_right == "left" ? "right" : "left";
 }
 
-function generate_html_from_services(services) {
+function generate_html_from_services(services, fill_height) {
   // console.log(JSON.stringify(services));
   var left_or_right = "left";
   var generated_html = "";
+  var height = fill_height ? "style='height: " + (100.0 / services.length) + "%' " : ""
   for (var i=0; i<services.length; ++i) {
     // console.log(JSON.stringify(services[i]));
-    generated_html += convert_service_entry_to_tr(services[i], left_or_right)
+    generated_html += convert_service_entry_to_tr(services[i], left_or_right, height)
     left_or_right = toggle_left_right(left_or_right);
   }
   return generated_html;
@@ -87,7 +89,7 @@ function display_nagios_status(data, t, j) {
   
   if (services_by_state["bad"] !== undefined) {
     html += "<div class='state-bad' style='height: " + (has_good_state ? "50%" : "100%") + "'>";
-    html += generate_html_from_services(services_by_state["bad"]);
+    html += generate_html_from_services(services_by_state["bad"], true);
     html += "</div>"
   }
   
