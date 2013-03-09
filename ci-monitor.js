5a30f89dbfdba3dbf787588f02652716f5e0ba74
Jamie Cook <jimi.cook@gmail.com>
2013-03-08 23:30:11 -0500

    refactor

--- ci-monitor.js
+++ ci-monitor.js
@@ -71,7 +71,7 @@ function display_nagios_status(data, t, j) {
       }
   }
   
-  var services_by_state = _.groupBy(service_entries, function (e) { return parseInt(e['current_state'], 10) })
+  var services_by_state = _.groupBy(service_entries, function (e) { return parseInt(e['current_state'], 10); })
   console.log(JSON.stringify(services_by_state))
   console.log(JSON.stringify(Object.keys(services_by_state)))
   
