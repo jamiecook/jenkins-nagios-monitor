725ed857c0ef3037fa4e640e551c98b721f61f76
Jamie Cook <jimi.cook@gmail.com>
2013-03-15 03:13:10 -0400

    refactor

--- ci-monitor.js
+++ ci-monitor.js
@@ -90,6 +90,7 @@ function display_nagios_status(data, t, j) {
   if (services_by_state["bad"] !== undefined) {
     var div_height_abs = $(window).height() / (has_good_state ? 2.0 : 1.0);
     var div_height_rel = 100.0 / (has_good_state ? 2.0 : 1.0);
+    console.log(JSON.stringify(services_by_state["bad"]))
     html += generate_html_from_services(services_by_state, "bad", div_height_abs, div_height_rel);
   }
   
